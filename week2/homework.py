a65a740ab63facbabd370a8dfe8de4233bd90f77
wtl-ghacc <70339434+wtl-ghacc@users.noreply.github.com>
2020-09-02 23:04:09 -0400

    Update homework.py

--- week2/homework.py
+++ week2/homework.py
@@ -2,10 +2,10 @@ import logging
 logging.basicConfig(level=logging.DEBUG)
 
 def squared_threes():
-    return_value = [range(0,100)]
+    return_value = [3,6,9,12,15,18,21,24,27,30,33,36,39,42,45,48,51,54,57,60,63,66,69,72,75,78,81,84,87,90,93,96,99]
     for i in return_value:
-        
-    # END SHOULDNT GO BEYOND HERE (PAST LINE 8)
+        print(i**2)
+
     return return_value
     
 if __name__ == "__main__":
